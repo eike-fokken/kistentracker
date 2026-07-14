b73ada3cbcaf3ed91cc4da50b784f6f26f2724bd
Eike Fokken <e.fokken+github@posteo.de>
2026-07-14 22:47:18 +0200

    fix mismatch of env var name.

--- dbtrials/dbtrials/settings.py
+++ dbtrials/dbtrials/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = os.getenv("SECRET_KEY", "change-me-in-production")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", "True").lower() in ("true", "1")
 
-ALLOWED_HOSTS: list[str] = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS: list[str] = os.getenv(
+    "DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1"
+).split(",")
 
 
 # Application definition
